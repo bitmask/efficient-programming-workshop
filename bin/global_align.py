6c3d9442e71c6543b43ec2728017ca630a2322e6
Alexander Junge <ajunge@rth.dk>
2015-05-01 22:44:49 +0200

    style fixes to be (almost) PEP8 compliant

--- bin/global_align.py
+++ bin/global_align.py
@@ -1,119 +1,128 @@
 # -*- coding: utf-8 -*-
 import Exceptions
 
-def fill_DPM(seq1, seq2, scoring_matrix, gap_penalty):
+
+def fill_dpm(seq1, seq2, scoring_matrix, gap_penalty):
     #Function to fill out the Dynamic Programming matrix for 2 given sequences
 
     #Initialize the DP_matrix
-    DPM = [[0]*(len(seq2)+1) for i in range(len(seq1)+1)]
+    dpm = [[0]*(len(seq2)+1) for i in range(len(seq1)+1)]
     
     # HERE COMES THE CODE #
     """
     PSEUDOCODE
         for i=0 : length(A)
-            DPM(i,0) ← (-1)*i
+            dpm(i,0) ← (-1)*i
 
         for j=0 to length(B)
-            DPM(0,j) ← (-1)*j
+            dpm(0,j) ← (-1)*j
 
         for i=1 to length(A)
             for j=1 to length(B)
             {
                 s =  1 if Ai = Bj, (-1) otherwise
-                Match ← DPM(i-1,j-1) + s
-                Delete ← DPM(i-1, j) - 1
-                Insert ← DPM(i, j-1) - 1
-                DPM(i,j) ← max(Match, Insert, Delete)
+                Match ← dpm(i-1,j-1) + s
+                Delete ← dpm(i-1, j) - 1
+                Insert ← dpm(i, j-1) - 1
+                dpm(i,j) ← max(Match, Insert, Delete)
              }
     """
 
-    return DPM
+    return dpm
 
-def backtrack_DPM(seq1, seq2, DPM, scoring_matrix, gap_penalty):
+
+def backtrack_dpm(seq1, seq2, dpm, scoring_matrix, gap_penalty):
     # Backtrack and construct the alignment from given DP_matrix
 
-    # Backtracking starts at the last cell of DP_matrix DPM[i][j]
-    i=len(seq1)
-    j=len(seq2)
+    # Backtracking starts at the last cell of DP_matrix dpm[i][j]
+    i = len(seq1)
+    j = len(seq2)
 
-    alignment=[] # alignment is currently empty
+    alignment = []  # alignment is currently empty
     
-    # i and j will represent our current position in the DPM
+    # i and j will represent our current position in the dpm
     # Iterate over i and j simultaneously, until one becomes 0
-    while i!=0 and j!=0:
+    while i != 0 and j != 0:
         chr1 = seq1[i-1]
         chr2 = seq2[j-1]
 
-        match = DPM[i-1][j-1]+scoring_matrix[chr1][chr2] # What would be the score of the cell if seq1[i] and seq2[j] are matched 
-        gap1 = DPM[i-1][j] - gap_penalty  # What would be the score if we have a gap aligned with seq1[i]
-        gap2 = DPM[i][j-1] - gap_penalty  # What would be the score if we have a gap aligned with seq2[j]
+        # What would be the score of the cell if seq1[i] and seq2[j] are matched
+        match = dpm[i-1][j-1] + scoring_matrix[chr1][chr2]
+
+        # What would be the score if we have a gap aligned with seq1[i]
+        gap1 = dpm[i-1][j] - gap_penalty
+
+        # What would be the score if we have a gap aligned with seq2[j]
+        gap2 = dpm[i][j-1] - gap_penalty
 
         # We have to trace back to the cell which will give the maximum score
         # if gap1 is maximum, seq1[i] is aligned to gap
         if gap1 >= match and gap1 >= gap2:
-            alignment.append((chr1,'-',' '))
-            i = i-1
+            alignment.append((chr1, '-', ' '))
+            i -= 1
  
        # if gap2 is maximum, seq2[j] is aligned to gap
         elif gap2 >= match and gap2 >= gap1:
-            alignment.append(('-',chr2,' '))
-            j = j-1
+            alignment.append(('-', chr2, ' '))
+            j -= 1
 
         # if match is maximum, seq1[i] and seq2[j] are aligned either as match or mismatch
         else:
-            if chr1==chr2:
-                alignment.append((chr1,chr2,'|')) #match
+            if chr1 == chr2:
+                alignment.append((chr1, chr2, '|'))  # match
             else:
-                alignment.append((chr1,chr2,':')) #mismatch
-            i = i-1
-            j = j-1
+                alignment.append((chr1, chr2, ':'))  # mismatch
+            i -= 1
+            j -= 1
 
     # since we hit the wall in DP (1st col or 1st row), there are just gaps left to align
-    while i!=0:
+    while i != 0:
         chr1 = seq1[i-1]
-        alignment.append((chr1,'-',' '))
-        i = i-1
-    while j!=0:
+        alignment.append((chr1, '-', ' '))
+        i -= 1
+    while j != 0:
         chr2 = seq2[j-1]
-        alignment.append(('-',chr2,' '))
-        j = j-1
+        alignment.append(('-', chr2, ' '))
+        j -= 1
 
     return alignment[::-1]
+
 
 def align(seq1, seq2, scoring_matrix, gap_penalty=2):
     # Aligns two given sequences with dynamic programming
     # Output is a list of 3-tuples (chr1,chr2,("|" for match, ":" for mismatch, " " for gap))
-    print "aligning two sequences: " 
-    print "Seq1:"+seq1 
-    print "Seq2:"+seq2
+    print("aligning two sequences: ")
+    print("Seq1:"+seq1)
+    print("Seq2:"+seq2)
 
     #Fill out the dynamic programming matrix
-    DPM = fill_DPM(seq1, seq2, scoring_matrix, gap_penalty)
+    dpm = fill_dpm(seq1, seq2, scoring_matrix, gap_penalty)
     #Backtrack on DPM and get the alignment
-    alignment = backtrack_DPM(seq1, seq2, DPM, scoring_matrix, gap_penalty)
+    alignment = backtrack_dpm(seq1, seq2, dpm, scoring_matrix, gap_penalty)
 
-    return alignment, DPM[-1][-1] # return the alignment and its score
+    return alignment, dpm[-1][-1]  # return the alignment and its score
+
 
 def print_alignment(alignment):
     # Prints the alignment
-    print "The final alignment is:\n"
-    line1=''
-    line2=''
-    line3=''
+    print("The final alignment is:\n")
+    line1 = ''
+    line2 = ''
+    line3 = ''
 
-    for (chr1,chr2,algn) in alignment:
+    for (chr1, chr2, algn) in alignment:
         line1 += chr1
         line2 += chr2
         line3 += algn
 
         #limit each line with 50 characters
-        if len(line1)==50:
-            print line1+'\n'+line2+'\n'+line3+'\n'
+        if len(line1) == 50:
+            print(line1+'\n'+line2+'\n'+line3+'\n')
             line1 = ''
             line2 = ''
             line3 = ''
 
-    print line1+'\n'+line2+'\n'+line3
+    print(line1 + '\n' + line2 + '\n' + line3)
     return True
 
 def get_scoring_matrix(request):
@@ -159,10 +168,9 @@ def get_scoring_matrix(request):
                                'Y':
                                 {'A':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'K':0, 'L':0, 'M':0, 'N':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'V':0, 'W':0, 'Y':1},
                               }, 
-                            'BLOSUM62': {} } #  TODO
+                             'BLOSUM62' : {}  # TODO
+                            }
     if request in avail_matrices.keys():
         return avail_matrices[request]
     else:
-         raise Exceptions.MissingMatrixType("Matrix type " + request + "does not exist")
-
-
+        raise Exceptions.MissingMatrixType("Matrix type " + request + "does not exist")
